31245a3cdff9798b86a3d6d9c87048f39f59ebe1
Pius Aaron <piuschbz@gmail.com>
2024-10-10 13:28:31 +0100

    Integrated Auth endpoints

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,6 +1,38 @@
 import sigupImage from "../assets/images/signup_image.png";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function SignUp() {
+  const [ fullName, setFullName] = useState("");
+  const [ email, setEmail ] = useState("");
+  const [ password, setPassword ] = useState("");
+  const [ confirmPaswword, setConfirmPaswword ] = useState("");
+  const navigate = useNavigate();
+  const [ error, setError ] = useState("");
+
+  async function handleSignUp(e) {
+    e.preventDefault();
+    if (confirmPaswword !== password){
+      setError("Passwords do not match");
+      return;
+    }
+    try{
+      const response = await axios.post('https://pova-blog.onrender.com/api/v1/auth/register', 
+        {
+          fullName, email, password
+        }
+      );
+
+      if (response.status == 201)
+        navigate('/signin');
+
+    } catch(err) {
+      console.log(err);
+      setError('Registration failed!');
+    }
+  }
+
   return (
     <div className="flex w-3/4 my-96 mx-auto font-prime_font">
       <div className="">
@@ -18,31 +50,48 @@ function SignUp() {
             <p className="text-2xl mb-2 font-semibold">Get Started With Pova</p>
             <p className="font-semibold text-xl text-gray-400">Get started is easy</p>
           </div>
-          <form action="">
+          <form onSubmit={handleSignUp}>
             <div className="flex flex-col">
               <input
                 type="text"
                 placeholder="Full Name"
                 className="font-medium w-96 p-5 my-5 rounded-lg border-gray-300 border-2"
+                value={fullName}
+                onChange={(e) => {
+                  setFullName(e.target.value);
+                }}
               />
               <input
                 type="email"
                 placeholder="Email"
                 className="font-medium w-96 p-5 my-5 rounded-lg border-gray-300 border-2"
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value)
+                }}
               />
               <input
                 type="password"
                 placeholder="Password"
                 className="font-medium w-96 p-5 my-5 rounded-lg border-gray-300 border-2"
+                value={password}
+                onChange={(e) => {
+                  setPassword(e.target.value)
+                }}
               />
               <input
                 type="password"
                 placeholder="Confirm Password"
                 className="font-medium w-96 p-5 my-5 rounded-lg border-gray-300 border-2"
+                value={confirmPaswword}
+                onChange={(e) => {
+                  setConfirmPaswword(e.target.value)
+                }}
               />
             </div>
+            {error && <p className="text-red-500">{error}</p>} {/* Display error */}
             <div>
-              <button className="text-center w-96 bg-orange_primary border-2 rounded-md p-3 my-9">
+              <button type="submit" className="text-center w-96 bg-orange_primary border-2 rounded-md p-3 my-9">
                 Create Account
               </button>
             </div>
--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,8 +1,48 @@
 import { useNavigate } from "react-router";
+import { useState } from "react";
+import axios from "axios";
 import siginImage from "../assets/images/signin-vector.png";
 
 function Signin() {
-  const navigate = useNavigate()
+  const [ email, setEmail ] = useState("");
+  const [ password, setPassword ] = useState("");
+  const navigate = useNavigate();
+  const [ error, setError ] = useState("");
+
+  async function handleLogin(e) {
+    e.preventDefault(); // prevents reload
+
+    if (!email){
+      setError("Provide your email");
+      return;
+    } else if (!password){
+      setError("Provide your password");
+      return;
+    }
+    try{
+      const response = await axios.post('https://pova-blog.onrender.com/api/v1/auth/login',
+        {
+          email,
+          password
+        }
+      );
+
+      if (response.data.accessToken){
+        localStorage.setItem('accessToken', response.data.accessToken);
+        navigate('/home');
+      }
+    } catch (err){
+      if (err.response && err.response.status === 401){
+        setError("Login failed! Incorrect password");
+      } else if (err.response && err.response.status === 404){
+        setError("Login failed! User account not found");
+      } else {
+        setError("Login Failed!");
+        console.error(err)
+      }
+    }
+  }
+
   return (
     <div className="flex w-3/4 p-0 static mx-auto font-prime_font">
       <div className="w-2/3 bg-custom_gray">
@@ -18,25 +58,30 @@ function Signin() {
             <p className="text-4xl font-semibold">Welcome Back</p>
             <p className="font-medium my-1">Login into your acount</p>
           </div>
-          <form action="">
+          <form onSubmit={handleLogin}>
             <div className="flex flex-col">
               <input
                 type="email"
                 placeholder="Email"
                 className="font-medium w-96 p-5 my-5 rounded-lg border-gray-300 border-2"
+                value={email}
+                onChange={(e) => { setEmail(e.target.value) }}
               />
               <input
                 type="password"
                 placeholder="Password"
                 className="font-medium w-96 p-5 my-5 rounded-lg border-gray-300 border-2"
+                value={password}
+                onChange={(e) => { setPassword(e.target.value) }}
               />
             </div>
+            {error && <p className="text-red-500">{error}</p>} {/* Display error */}
             <div className="flex justify-between">
               <p>Remember me</p>
               <p className="text-orange_primary">Forgot Password?</p>
             </div>
             <div>
-              <button onClick={()=>navigate('/home')} className="text-center w-96 border-gray-500 border-2 rounded-md p-3 my-9">
+              <button type="submit" className="text-center w-96 border-gray-500 border-2 rounded-md p-3 my-9">
                 Log In
               </button>
             </div>
